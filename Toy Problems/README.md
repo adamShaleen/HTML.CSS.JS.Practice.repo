58fd250b7788a337fee82ed6b396dcd9d186d84b
adamShaleen <adamshaleen@gmail.com>
2016-03-30 18:00:45 -0600

    adding toy repo

---
+++ Toy Problems/README.md
@@ -0,0 +1,11 @@
+This repo is meant for the mentors' use. If you're a student, goooo awwaayyyyyyy.
+
+I'll be updating this regularly as I sort through more stuff.
+
+PRs welcome, folks. And issues/bug reports/suggestions. Come at me. Brah.
+
+See also:
+* [node drills](https://github.com/zacanger/node-drills)
+* [and basically all my other repos](https://github.com/zacanger?tab=repositories)
+  (there are a lot of examples, boilerplates, demos, and such)
+
--- week2/day3/day.3.practice.js
+++ week2/day3/day.3.practice.js
@@ -39,3 +39,152 @@ console.log(newNames);
 //You can put a prototype wherever you want.
 
 //MDN has lots of great built in prototypes you can use and explanations.
+
+
+
+// ClOSURES
+// 1. a nested child function can access the parent functions
+// 2. we take a snapshot in time of the parent variables.
+// A factory that creates a factory analogy.
+
+// COLOR factory
+
+  function colorFactoryFactory(colorName) {
+      return function paintCanMaker() {
+          console.log('here is a ' + colorName + ' paint can.')
+      }
+      return paintCanMaker;
+  }
+
+  var redPaintCanFactory = colorFactoryFactory('red');
+  var bluePaintCanFactory = colorFactoryFactory('blue');
+  var greenPaintCanFactory = colorFactoryFactory('green');
+
+  redPaintCanFactory();
+  bluePaintCanFactory();
+  greenPaintCanFactory();
+
+//---------------------------------------------------------------------------
+
+
+function calorieTracker(baselineCalories) {
+    // keep track of totalCalories today
+    var totalCaloriesToday = 0;
+    var totalDays = 1;
+    // keep track of totalCalories all time
+    var totalCaloriesAllTime = 0;
+    // keep track of personalBaseLineCalories
+      //in parameter
+    //addCalories()
+    function reportEating(totalCaloriesConsumed) {
+        totalCaloriesToday += totalCaloriesConsumed;
+    }
+    //startNewDay()
+    function startNewDay() {
+        totalCaloriesAllTime += totalCaloriesToday;
+        totalCaloriesToday = 0;
+    }
+    //estimated weight loss today()
+    function getTodaysWeightLoss() {
+        //3500 calories = 1 lb
+        return totalCaloriesToday - baselineCalories / 3500;
+    }
+    //estimated weight loss all time()
+    function getAllTimeWeightLoss() {
+        //3500 calories = 1 lb
+        //today - baseline / 3500
+        return (totalCaloriesToday - (baselineCalories * totalDays)) / 3500;
+    }
+
+    return {
+        reportEating: reportEating,
+        startNewDay: startNewDay,
+        getTodaysWeightLoss: getTodaysWeightLoss,
+        getAllTimeWeightLoss: getAllTimeWeightLoss
+    }
+}
+
+var juansTracker = calorieTracker(2300);
+var pingsTracker = calorieTracker(1800);
+var sumoTracker = calorieTracker(8500);
+
+//-----------------------------------------------------------------------------
+
+// OBJECT MANIPULATION EXAMPLES
+
+var cat = {
+  name: 'Dinah',
+  type: 'Calico',
+  weight: '34 mice',
+  modd: 'grumpy',
+  color: 'snickers'
+}
+
+// dot notation
+// get- NOT ON THE LEFT SIDE OF A SINGLE EQUALS MARK.
+if(cat.weight > 17) {
+  console.log(cat.weight);
+}
+
+var catWeight = cat.weight;
+
+function forSomeReasonWeCareAboutCatWeight(cat.weight) {
+  console.log('we care and your cat weight ' + weight);
+}
+
+forSomeReasonWeCareAboutCatWeight(cat.weight);
+
+// set- WHEN ON THE LEFT SIDE OF A SINGLE EQUALS MARK.
+cat.weight = 32;
+
+//bracket notation
+// GET
+
+if(cat['weight'] > 17) {
+  console.log(cat.weight);
+}
+
+var catWeight = cat['weight'];
+
+//SET
+
+cat['weight'] = 32;
+
+
+// New object EXAMPLES
+
+var outfit = {
+  shirt: {
+    color: 'blue',
+    size: 'medium',
+    style: 'spandex',
+  },
+  pants: {
+    color: 'oranges',
+    size: 'XS',
+    style: 'denim'
+  }
+};
+
+
+//get
+outfit.shirt.color;
+
+if (outfit.pants.size === 'XS') {
+  console.log('cringe');
+}
+
+//set
+ outfit.shirt.style = 'cotton tee';
+
+ //Another objects
+ var calc = {
+  total: 0
+}
+
+calc.total += 7;
+calc.total -= 3;
+calc.total = (calc.total * 33)
+calc.total = calc.total / 100
+
+console.log(calc.total)
